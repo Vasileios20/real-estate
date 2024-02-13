3bb75306925e5e67d3445f93f23b0b6e3c2eee6a
vasileios20 <vasilis_tsim@outlook.com>
2024-02-13 11:48:05 +0000

    Update styles to NotFound page for responsive

--- src/components/NotFounds.js
+++ src/components/NotFounds.js
@@ -1,12 +1,16 @@
 import React from "react";
 import NoResults from "../assets/404-not-found.png";
-import Asset from "./Asset";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 
 const NotFound = () => {
   return (
-    <div>
-      <Asset src={NoResults} />
-    </div>
+    <Row>
+      <Col className="d-flex justify-content-center">
+        <Image src={NoResults} className="img-fluid" />
+      </Col>
+    </Row>
   );
 };
 
