ef05d055f3b413adda7bf008021957d055ace983
vasileios20 <vasilis_tsim@outlook.com>
2024-02-18 14:33:17 +0000

    Add scrollableTarget to InfiniteScroll to ListingsWhislistPage

--- src/components/ListingsWishlistPage.js
+++ src/components/ListingsWishlistPage.js
@@ -21,87 +21,97 @@ const ListingsWishlistPage = ({
   return (
     <>
       <SearchBar />
-      <Row className="mt-3">
-        {listings ? (
-          hasLoaded ? (
+      <Container
+        id="scrollableDiv"
+        style={{
+          height: 500,
+          overflow: "auto",
+          marginTop: "20px",
+        }}
+      >
+        <Row>
+          {listings ? (
+            hasLoaded ? (
+              <>
+                {array.length ? (
+                  <InfiniteScroll
+                    dataLength={array.length}
+                    loader={<Asset spinner />}
+                    hasMore={!!listings.next}
+                    next={() => fetchMoreData(listings, setListings)}
+                    scrollableTarget="scrollableDiv"
+                  >
+                    {array.map((listing) => (
+                      <Row key={listing.id} className="mx-0 align-items-center">
+                        <Col xs={12} md={6} lg={5} xl={4} className="p-0">
+                          <Link to={`/listings/${listing.id}`}>
+                            <Image
+                              src={listing.images[0].url}
+                              alt={listing.images[0].id}
+                              className={`img-fluid ${styles.Listingswishlist__Image}`}
+                            ></Image>
+                          </Link>
+                        </Col>
+                        <Col xs={12} md={6} lg={5} xl={4} className="mt-2">
+                          <Link to={`/listings/${listing.id}`}>
+                            <ListingHeader
+                              {...listing}
+                              listingPage={true}
+                              setListings={setListings}
+                            />
+                          </Link>
+                        </Col>
+                      </Row>
+                    ))}
+                  </InfiniteScroll>
+                ) : (
+                  <Container>
+                    <Row className="justify-content-center">{message}</Row>
+                    <Asset text="No results" />
+                  </Container>
+                )}
+              </>
+            ) : (
+              <Container>
+                <Asset spinner />
+              </Container>
+            )
+          ) : (
             <>
               {array.length ? (
-                <InfiniteScroll
-                  dataLength={array.length}
-                  loader={<Asset spinner />}
-                  hasMore={!!listings.next}
-                  next={() => fetchMoreData(listings, setListings)}
-                >
-                  {array.map((listing) => (
-                    <Row key={listing.id} className="mx-0 align-items-center">
-                      <Col xs={12} md={6} lg={5} xl={4} className="p-0">
-                        <Link to={`/listings/${listing.id}`}>
-                          <Image
-                            src={listing.images[0].url}
-                            alt={listing.images[0].id}
-                            className={`img-fluid ${styles.Listingswishlist__Image}`}
-                          ></Image>
-                        </Link>
-                      </Col>
-                      <Col xs={12} md={6} lg={5} xl={4} className="mt-2">
-                        <Link to={`/listings/${listing.id}`}>
+                array.map((listing) => (
+                  <Row key={listing.id} className="mx-0 align-items-center">
+                    <Col xs={12} md={6} lg={5} xl={4} className="px-0">
+                      <Link to={`/listings/${listing.id}`}>
+                        <Image
+                          src={listing.images[0].url}
+                          alt={listing.images[0].id}
+                          className={`img-fluid ${styles.Listingswishlist__Image}`}
+                        ></Image>
+                      </Link>
+                    </Col>
+                    <Col xs={12} md={6} lg={5} xl={4} className="mt-2">
+                      <Link to={`/listings/${listing.id}`}>
+                        <div>
                           <ListingHeader
                             {...listing}
                             listingPage={true}
                             setListings={setListings}
                           />
-                        </Link>
-                      </Col>
-                    </Row>
-                  ))}
-                </InfiniteScroll>
+                        </div>
+                      </Link>
+                    </Col>
+                  </Row>
+                ))
               ) : (
                 <Container>
-                  <Row className="justify-content-center">{message}</Row>
                   <Asset text="No results" />
                 </Container>
               )}
             </>
-          ) : (
-            <Container>
-              <Asset spinner />
-            </Container>
-          )
-        ) : (
-          <>
-            {array.length ? (
-              array.map((listing) => (
-                <Row key={listing.id} className="mx-0 align-items-center">
-                  <Col xs={12} md={6} lg={5} xl={4} className="px-0">
-                    <Link to={`/listings/${listing.id}`}>
-                      <Image
-                        src={listing.images[0].url}
-                        alt={listing.images[0].id}
-                        className={`img-fluid ${styles.Listingswishlist__Image}`}
-                      ></Image>
-                    </Link>
-                  </Col>
-                  <Col xs={12} md={6} lg={5} xl={4} className="mt-2">
-                    <Link to={`/listings/${listing.id}`}>
-                      <div>
-                        <ListingHeader
-                          {...listing}
-                          listingPage={true}
-                          setListings={setListings}
-                        />
-                      </div>
-                    </Link>
-                  </Col>
-                </Row>
-              ))
-            ) : (
-              <Container>
-                <Asset text="No results" />
-              </Container>
-            )}
-          </>
-        )}
-      </Row>
+          )}
+        </Row>
+      </Container>
     </>
   );
 };
