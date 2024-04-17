a61f23b1716664f8f7707279f86b0318d8cf5428
vasileios20 <vasilis_tsim@outlook.com>
2024-04-17 18:06:53 +0100

    Update footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,113 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../styles/Footer.module.css";
+import { axiosReq } from "../api/axiosDefaults";
+import { Form } from "react-bootstrap";
 
 const Footer = () => {
   /**
    * The Footer component is a functional component that renders the footer of the application.
    */
+
+  const history = useHistory();
+
+  const handleSubmit = async (selectedType) => {
+    // Fetch the listings from the API using the search parameters.
+    let path = `/listings/?type=${selectedType}`;
+
+    try {
+      const { data } = await axiosReq.get(`${path}`);
+      console.log(path);
+      console.log("data", data);
+      history.push(`${path}`, { data: data });
+    } catch (err) {
+      // console.log(err);
+      if (err.response.status === 400) {
+      }
+    }
+  };
+
   return (
-    <Container fluid className="mt-5">
-      <Row
-        className={`justify-content-center align-items-center ${styles.Footer}`}
-      >
-        <Col>
-          <Row className="flex-column justify-content-end align-items-center">
-            <h5>CB Real Estate</h5>
-            <Link to="/about">About Us</Link>
-            <Link to="/contact">Contact</Link>
-            <Link to="/listings">Listings</Link>
-          </Row>
-        </Col>
-        <Col className="d-flex justify-content-center">
-          <>
-            <a
-              className="p-1"
-              href="https://github.com/Vasileios20/real-estate"
-              target="_blank"
-              aria-label="Visit the game's repository page on git hub"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-github"></i>
-            </a>
-            <a
-              className="p-1"
-              href="https://www.linkedin.com/in/vasileios-tsimourdagkas/"
-              target="_blank"
-              aria-label="Visit developers LinkedIn profile"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-linkedin"></i>
-            </a>
-          </>
-        </Col>
-      </Row>
+    <Container fluid className={`${styles.Footer} mt-4`}>
+      <Container>
+        <Row className="pt-3">
+          <Col className="">
+            <h5>Acropolis Real Estate</h5>
+            <Link to="/about" className={`${styles.link}`}>
+              About Us |
+            </Link>
+            <Link to="/contact" className={`${styles.link} ps-1`}>
+              Contact |
+            </Link>
+            <Link to="/faq" className={`${styles.link} ps-1`}>
+              FAQ
+            </Link>
+          </Col>
+
+          <Col className="d-flex flex-column">
+            <Row>
+              <Col>
+                <h5>Properties</h5>
+              </Col>
+            </Row>
+            <Row className="justify-content-end">
+              <Col className="d-flex">
+                <Form.Label
+                  className={`${styles.pointer} ${styles.link}`}
+                  onClick={() => handleSubmit("apartment")}
+                >
+                  Apartment |{" "}
+                </Form.Label>
+
+                <Form.Label
+                  className={`${styles.pointer} ${styles.link} ps-1`}
+                  onClick={() => handleSubmit("house")}
+                >
+                  House |{" "}
+                </Form.Label>
+
+                <Form.Label
+                  className={`${styles.pointer} ${styles.link} ps-1`}
+                  onClick={() => handleSubmit("land")}
+                >
+                  Land |{" "}
+                </Form.Label>
+
+                <Form.Label
+                  className={`${styles.pointer} ${styles.link} ps-1`}
+                  onClick={() => handleSubmit("commercial")}
+                >
+                  Commercial
+                </Form.Label>
+              </Col>
+            </Row>
+          </Col>
+
+        </Row>
+        <Row className="text-start mt-2">
+          <hr />
+          <Col sm={4}>
+            <p className="text-muted">
+              &copy; 2021 Acropolis Real Estate. All rights reserved.
+            </p>
+          </Col>
+          <Col sm={4} className="">
+            <p className="text-muted">
+              <Link to="/cookies">Cookies </Link> | <Link to="/privacyPolicy">Privacy policy</Link> | <Link to="/terms">Terms & Conditions</Link>
+            </p>
+          </Col>
+
+          <Col >
+            <>
+              <p className="text-muted" style={{ fontSize: "12px" }}>Developed by
+              </p>
+            </>
+          </Col>
+        </Row>
+      </Container>
     </Container>
   );
 };
