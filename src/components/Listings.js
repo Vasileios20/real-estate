bbeac43f138a8e6b9ca3cc25dcaf9cd0f9a70f96
vasileios20 <vasilis_tsim@outlook.com>
2024-04-29 21:25:36 +0100

    Update styles to listings' card to improve responsive UX

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -1,7 +1,6 @@
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-// import Image from "react-bootstrap/Image";
 import styles from "../styles/Listing.module.css";
 
 import Asset from "./Asset";
@@ -13,8 +12,6 @@ import SearchBar from "./SearchBar";
 import Card from "react-bootstrap/Card";
 import Carousel from "react-bootstrap/Carousel";
 import { APIProvider, AdvancedMarker, Map } from "@vis.gl/react-google-maps";
-import homeStyles from "../styles/HomePage.module.css";
-import ListingImages from "../pages/listings/ListingImages";
 
 const ListingsPage = ({ array, hasLoaded, setListings, listings, message }) => {
   // The ListingsPage component is a functional component that renders the listings from the database.
@@ -34,9 +31,8 @@ const ListingsPage = ({ array, hasLoaded, setListings, listings, message }) => {
 
     for (let i = 0; i < array.length; i += 2) {
       const cardsInRow = array.slice(i, i + 2).map((listing) => (
-        <Col key={listing.id} className="my-2 px-0 mx-0">
+        <Col key={listing.id} className={`my-2 ${styles.colCard}`}>
           <Card
-            // style={{ width: "18rem", height: "100%" }}
             className={`rounded p-0 ${styles.Listings__Card}`}
           >
             <Link to={`/listings/${listing.id}`}>
@@ -53,7 +49,6 @@ const ListingsPage = ({ array, hasLoaded, setListings, listings, message }) => {
                   </Carousel.Item>
                 ))}
               </Carousel>
-              {/* <ListingImages images={listing.images} /> */}
               <div style={{ marginLeft: "10px" }}>
                 <ListingHeader
                   {...listing}
@@ -66,7 +61,7 @@ const ListingsPage = ({ array, hasLoaded, setListings, listings, message }) => {
         </Col>
       ));
       cardRow.push(
-        <Row key={i} className="justify-content-start mx-0">
+        <Row key={i} className="mx-0">
           {cardsInRow}
         </Row>
       );
@@ -74,11 +69,6 @@ const ListingsPage = ({ array, hasLoaded, setListings, listings, message }) => {
 
     return cardRow;
   };
-  console.log("array", array);
-  const oddArray = array.length % 2 === 0;
-  console.log("oddArray", oddArray);
-  const lastRow = array.length - 2;
-  console.log("lastRow", lastRow);
 
 
   const listingMapMarkers = latLng.map((listing, index) => (
@@ -143,91 +133,6 @@ const ListingsPage = ({ array, hasLoaded, setListings, listings, message }) => {
         </Row>
       </Container>
     </>
-    // <>
-    //   <SearchBar />
-    //   <Container fluid style={{ marginTop: "20px" }}>
-    //     <Row className="justify-content-around">
-    //       <Col xs={12} sm={12} md={10} lg={7}>
-    //         <Container
-    //           id="scrollableDiv"
-    //           style={{ height: 500, overflow: "auto" }}
-    //         >
-    //           {hasLoaded ? (
-    //             <>
-    //               {/* {array.length ? (
-    //                 <InfiniteScroll
-    //                   children={renderCardRow()}
-    //                   dataLength={array.length}
-    //                   loader={<Asset spinner />}
-    //                   hasMore={!!listings.next}
-    //                   next={() => fetchMoreData(listings, setListings)}
-    //                 /> */}
-    //               {listings.results.length ? (
-    //                 <Row xs={1} md={2} className="g-4">
-    //                   {Array.from(listings.results).map((listing) => (
-    //                     <Col key={listing.id}>
-    //                       <Link to={`/listings/${listing.id}`}>
-    //                         <Card style={{ height: "100%" }}>
-    //                           <Carousel indicators={false}>
-    //                             {listing.images.map((image, index) => (
-    //                               <Carousel.Item
-    //                                 key={index}
-    //                                 style={{ height: "200px" }}
-    //                               >
-    //                                 <Card.Img
-    //                                   className={`${styles.Listings__Image}`}
-    //                                   src={image.url}
-    //                                   alt={image.id}
-    //                                   variant="top"
-    //                                 // style={{ height: "200px" }}
-    //                                 />
-    //                               </Carousel.Item>
-    //                             ))}
-    //                           </Carousel>
-
-    //                           <Card.Body>
-    //                             <ListingHeader
-    //                               {...listing}
-    //                               listingPage={true}
-    //                             />
-    //                           </Card.Body>
-    //                         </Card>
-    //                       </Link>
-    //                     </Col>
-    //                   ))}
-    //                 </Row>
-    //               ) : (
-    //                 <Container>
-    //                   {message}
-    //                 </Container>
-    //               )}
-    //             </>
-    //           ) : (
-    //             <Container>
-    //               <Asset spinner />
-    //             </Container>
-    //           )}
-    //         </Container>
-    //       </Col>
-    //       <Col xs={10} lg={5} className="mt-3 mt-lg-0">
-    //         <APIProvider apiKey={API_KEY}>
-    //           <Map
-    //             mapId={"bf51a910020fa25a"}
-    //             defaultZoom={8}
-    //             defaultCenter={{
-    //               lat: 51.50898721256282,
-    //               lng: -0.11773481844149021,
-    //             }}
-    //             gestureHandling={"greedy"}
-    //             style={{ width: "100%", height: "500px" }}
-    //           >
-    //             {listingMapMarkers}
-    //           </Map>
-    //         </APIProvider>
-    //       </Col>
-    //     </Row>
-    //   </Container>
-    // </>
   );
 };
 
