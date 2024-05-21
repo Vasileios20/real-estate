a7b6979edd948da3b9e821c4b4185b7bc837e174
vasileios20 <vasilis_tsim@outlook.com>
2024-05-21 17:05:59 +0100

    refactor: Update ListingsPage to display approved listings and condtional the heroImg

--- src/components/Listings.js
+++ src/components/Listings.js
@@ -14,56 +14,64 @@ import Card from "react-bootstrap/Card";
 import Carousel from "react-bootstrap/Carousel";
 import { APIProvider, AdvancedMarker, Map } from "@vis.gl/react-google-maps";
 
-const ListingsPage = ({ array, hasLoaded, setListings, listings, message }) => {
+const ListingsPage = ({ array, hasLoaded, setListings, listings, message, searchResults }) => {
   // The ListingsPage component is a functional component that renders the listings from the database.
   // It also renders the results of the search bar. The component uses the InfiniteScroll component to
   //  display the listings in an infinite scroll.
   // If the listings are not loaded, the component displays a spinner. If there are no results, the component displays a message.
   // The component also uses the SearchBar component to display the search bar at the top of the page.
 
+
+  const approvedListings = listings.results.filter((listing) => listing.approved === true);
   // Get the lat and lng from the listings and push it in the array.
-  const latLng = array.map((listing) => ({
+  const latLng = approvedListings.map((listing) => ({
     lat: listing.latitude,
     lng: listing.longitude,
   }));
 
+  const fetchMoreApprovedData = async () => {
+    // Fetch more data but only the approved listings
+    const moreData = await fetchMoreData(listings, setListings);
+    const approvedMoreData = moreData.results.filter((listing) => listing.approved === true);
+    return approvedMoreData;
+  };
 
 
   const listingMapMarkers = latLng.map((listing, index) => (
     <AdvancedMarker key={index} position={listing} />
   ));
 
-
   const API_KEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
 
   return (
     <>
-      {/* <div className={` d-flex flex-column ${heroStyles.HeroImageListings}`}>
+      {!searchResults && <div className={` d-flex flex-column ${heroStyles.HeroImageListings}`}>
+
         <h1 className={styles.Welcome} style={{ color: "#fff" }}>Properties</h1>
         <SearchBar />
-      </div> */}
+      </div>}
       <Container fluid className="px-lg-5 pt-5">
-        <SearchBar />
+        {searchResults && <SearchBar />}
         <Row className="mt-1 justify-content-around">
           <Col xs={12} lg={12} xl={7}>
             <Container
               id="scrollableDiv"
-              style={{ height: 400, overflow: "auto" }}
+              style={{ height: 800, overflow: "auto" }}
               className=""
             >
               {hasLoaded ? (
                 <>
-                  {array.length ? (
+                  {approvedListings.length ? (
                     <InfiniteScroll
 
-                      dataLength={array.length}
+                      dataLength={approvedListings.length}
                       loader={<Asset spinner />}
                       hasMore={!!listings.next}
-                      next={() => fetchMoreData(listings, setListings)}
+                      next={fetchMoreApprovedData}
                       scrollableTarget="scrollableDiv"
                     >
                       <Row className="mx-0">
-                        {array.map((listing) => (
+                        {approvedListings.map((listing) => (
                           <>
                             <Col key={listing.id} xs={12} md={6} lg={4} xl={4} className="mb-3 gx-1">
                               <Card style={{ height: "100%" }}>
@@ -111,20 +119,20 @@ const ListingsPage = ({ array, hasLoaded, setListings, listings, message }) => {
             {/* <hr className="" /> */}
           </Col>
           <Col sm={12} lg={5} className="d-none d-xl-block">
-            <APIProvider apiKey={API_KEY}>
-              <Map
-                mapId={"bf51a910020fa25a"}
-                defaultZoom={10}
-                defaultCenter={{
-                  lat: 51.50898721256282,
-                  lng: -0.11773481844149021,
-                }}
-                gestureHandling={"greedy"}
-                style={{ width: "100%", height: "400px" }}
-              >
-                {listingMapMarkers}
-              </Map>
-            </APIProvider>
+              <APIProvider apiKey={API_KEY}>
+                <Map
+                  mapId={"bf51a910020fa25a"}
+                  defaultZoom={10}
+                  defaultCenter={{
+                    lat: 51.50898721256282,
+                    lng: -0.11773481844149021,
+                  }}
+                  gestureHandling={"greedy"}
+                  style={{ width: "100%", height: "400px" }}
+                >
+                  {listingMapMarkers}
+                </Map>
+              </APIProvider>
           </Col>
         </Row>
       </Container >
