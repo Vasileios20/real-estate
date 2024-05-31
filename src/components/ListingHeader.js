ac68870fe9b750bb5b3c26aae8cf8a7e984f7511
vasileios20 <vasilis_tsim@outlook.com>
2024-05-31 15:29:12 +0100

    refactor: Update ListingHeader component to use translated sale type and improve styles

--- src/components/ListingHeader.js
+++ src/components/ListingHeader.js
@@ -2,41 +2,27 @@ import React, { useEffect } from "react";
 
 import styles from "../styles/Listing.module.css";
 import { useTranslation } from "react-i18next";
-import { Card } from "react-bootstrap";
 
 const ListingHeader = (props) => {
-  /**
-   * This component is used to display the header of a listing.
-   * It receives the following props:
-   * - type: string
-   * - sale_type: string
-   * - description: string
-   * - address_number: string
-   * - address_street: string
-   * - postcode: string
-   * - city: string
-   */
 
-  const saleType = props.sale_type === "rent" ? "rent" : "buy";
   const { t, i18n } = useTranslation();
 
   useEffect(() => {
     const lng = navigator.language || navigator.userLanguage;
     i18n.changeLanguage(lng);
   }, [i18n]);
+
+  const saleType = props.sale_type === "rent" ? `${t("propertyDetails.typeRent")}` : `${t("propertyDetails.typeSale")}`;
+
   return (
-    <Card.Body>
+    <div className={styles.Listing__cardBody}>
       <div className={styles.Listing__header}>
-        <h5 style={{ textTransform: "capitalize" }}>
-          {/* {props.id} */}
-          {/* {props.type} to {saleType} */}
-          <p>
-            {t("propertyDetails.title", {
-              type: `${props.type}`,
-              sale_type: `${saleType}`,
-            })}
-          </p>
-        </h5>
+        <div className={styles.Listing__headerListingDetails}>
+          {t("propertyDetails.title", {
+            type: props.type === "residential" || props.type === "commercial" ? props.sub_type : props.type,
+            sale_type: saleType,
+          })}, {props.municipality}, {props.county}, {props.region}
+        </div>
         <div className={styles.Listing__fontawsome}>
           <p>
             <i className="fa-solid fa-bed"> {props.bedrooms}</i>
@@ -48,15 +34,9 @@ const ListingHeader = (props) => {
             <i className="fa-solid fa-stairs"> {props.floor}</i>
           </p>
         </div>
-        {/* <h3>Description</h3>
-        <p>{props.description}</p> */}
-        <h6>{t("propertyDetails.address")}: </h6>
-        <p>
-          {props.address_number}, {props.address_street}, {props.postcode},{" "}
-          {props.city}
-        </p>
+        <h6 className={styles.Listing__price}>{t("propertyDetails.price")}: {props.currency} {props.price}</h6>
       </div>
-    </Card.Body>
+    </div>
   );
 };
 
