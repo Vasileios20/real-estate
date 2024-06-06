a46ff9582c3d547599d03e5e7a27058a69f073fb
vasileios20 <vasilis_tsim@outlook.com>
2024-06-06 20:11:11 +0100

    refactor: Update AboutPage to use translations for titles and list items

--- src/pages/home/AboutPage.js
+++ src/pages/home/AboutPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { useTranslation } from "react-i18next";
+import { Trans, useTranslation } from "react-i18next";
 import homeStyles from "../../styles/HomePage.module.css";
 import styles from "../../styles/ServicesPages.module.css";
 
@@ -23,7 +23,7 @@ export default function AboutPage() {
     <>
       <div className={`${homeStyles.HeroImageAbout}`}>
         <div className={`col-6 ${styles.HeaderContainer}`}>
-          <h1 className={`${styles.Header}`}>About us</h1>
+          <h1 className={`${styles.Header}`}>{t("aboutPage.title")}</h1>
         </div>
 
       </div>
@@ -36,29 +36,31 @@ export default function AboutPage() {
             </div>
           </Row>
         </Row>
-        <p className="h2 text-center pt-3">Why Us</p>
+        <p className="h2 text-center pt-3">{t("aboutPage.ListTitle")}</p>
         <Row className="mx-auto mb-5 shadow">
 
           <div className={styles.PageList}>
             <ul className={styles.List}>
-              <li><strong>Experience:</strong> With years of experience, our team brings a wealth of knowledge, expertise, and insight to every aspect of real estate industry.
-              </li>
-              <li><strong>Integrity:</strong> We conduct our business with the highest standards of integrity, honesty, and transparency, earning the trust and respect of our clients and partners.
-              </li>
-              <li><strong>Customer Focus:</strong> Our clients are our top priority, and we are dedicated to providing personalized attention, tailored solutions, and exceptional service to meet their unique needs.</li>
-              <li><strong>Commitment to Excellence:</strong> We are committed to excellence in everything we act, continuously striving to exceed expectations and deliver outstanding results for our clients.
-              </li>
-              <li><strong>Expertise:</strong> Highly educated professionals with years of experience in the real estate sector.
-              </li>
+              <li><Trans i18nKey="aboutPage.listItem1">{t("aboutPage.listItem1")}
+              </Trans></li>
+              <li>
+                <Trans i18nKey="aboutPage.listItem2">{t("aboutPage.listItem2")}
+                </Trans></li>
+              <li><Trans i18nKey="aboutPage.listItem3">{t("aboutPage.listItem3")}
+              </Trans></li>
+              <li><Trans i18nKey="aboutPage.listItem4">{t("aboutPage.listItem4")}
+              </Trans></li>
+              <li><Trans i18nKey="aboutPage.listItem5">{t("aboutPage.listItem5")}
+              </Trans></li>
             </ul>
           </div>
         </Row>
 
-        <p className="h2 text-center pt-3">Our Team</p>
+        <p className="h2 text-center pt-3">{t("aboutPage.bottomTitle")}</p>
         <Row className={`shadow mx-auto ${styles.PageContentWrapper}`}>
           <Row className={`mx-auto ${styles.PageContent}`}>
             <div className="my-5 col-11 mx-auto">
-              At Acropolis Estates, we have assembled a team of highly skilled and dedicated professionals who are passionate about real estate and dedicated to helping our clients succeed. From our experienced agents to our dedicated support staff, each member of our team is committed to providing exceptional service and unparalleled expertise. We are committed to providing personalized attention, expert advice, and unparalleled customer service to ensure a seamless and stress-free experience.
+              {t("aboutPage.bottomDescription")}
             </div>
           </Row>
         </Row>
