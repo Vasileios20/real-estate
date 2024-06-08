7b40afd889411713cc53da6df7b3541f357642e0
vasileios20 <vasilis_tsim@outlook.com>
2024-06-08 22:04:06 +0100

    refactor: Adjust margin-top in ListingCreateEditForm for better layout

--- src/pages/listings/ListingCreateForm.js
+++ src/pages/listings/ListingCreateForm.js
@@ -8,7 +8,6 @@ import Alert from "react-bootstrap/Alert";
 import Image from "react-bootstrap/Image";
 
 import upload from "../../assets/upload.png";
-
 import styles from "../../styles/ListingCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
@@ -22,17 +21,10 @@ import useUserStatus from "../../hooks/useUserStatus";
 import Forbidden403 from "../errors/Forbidden403";
 
 function ListingCreateForm() {
-  /**
-   * The ListingCreateForm component is a functional component that renders a form for creating a new listing.
-   * Imports the ListingTextFields component to render the input fields for the listing data.
-   * It a form for uploading images of the listing.
-   * @returns {JSX.Element} - The JSX for the component.
-   */
-
   useRedirect("loggedOut");
   const userStatus = useUserStatus();
   const [listingData, setListingData] = useState({
-    type: "apartment",
+    type: "",
     sale_type: "rent",
     description: "",
     address_number: "",
@@ -53,40 +45,13 @@ function ListingCreateForm() {
     availability: "",
     images: "",
     uploaded_images: [],
+    amenities: [],
   });
 
-  const {
-    type,
-    sale_type,
-    description,
-    address_number,
-    address_street,
-    postcode,
-    city,
-    price,
-    surface,
-    levels,
-    bedrooms,
-    floor,
-    kitchens,
-    bathrooms,
-    living_rooms,
-    heating_system,
-    energy_class,
-    construction_year,
-    availability,
-    images,
-  } = listingData;
-  // Set the errors state to an empty object.
   const [errors, setErrors] = useState({});
-
-  // Create a ref for the image input field.
   const imageInput = useRef(null);
-
-  // Get the history object from the useHistory hook.
   const history = useHistory();
 
-  // Handle the change event for the input fields.
   const handleChange = (e) => {
     setListingData({
       ...listingData,
@@ -94,10 +59,9 @@ function ListingCreateForm() {
     });
   };
 
-  // Handle the change event for the image input field.
   const handleChangeImage = (e) => {
     if (e.target.files.length) {
-      URL.revokeObjectURL(images);
+      URL.revokeObjectURL(listingData.images);
       setListingData({
         ...listingData,
         images: URL.createObjectURL(e.target.files[0]),
@@ -105,141 +69,88 @@ function ListingCreateForm() {
     }
   };
 
-  // Handle the submit event for the form.
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("type", type);
-    formData.append("sale_type", sale_type);
-    formData.append("description", description);
-    formData.append("address_number", address_number);
-    formData.append("address_street", address_street);
-    formData.append("postcode", postcode);
-    formData.append("city", city);
-    formData.append("price", price);
-    formData.append("surface", surface);
-    formData.append("levels", levels);
-    formData.append("bedrooms", bedrooms);
-    formData.append("floor", floor);
-    formData.append("kitchens", kitchens);
-    formData.append("bathrooms", bathrooms);
-    formData.append("living_rooms", living_rooms);
-    formData.append("heating_system", heating_system);
-    formData.append("energy_class", energy_class);
-    formData.append("construction_year", construction_year);
-    formData.append("availability", availability);
-    formData.append("images", imageInput.current.files[0]);
-    // Loop through the files in the image input field and append them to the formData object.
-    Array.from(imageInput.current.files).forEach((file) => {
-      formData.append("uploaded_images", file);
+    Object.keys(listingData).forEach((key) => {
+      if (key === "uploaded_images") {
+        Array.from(imageInput.current.files).forEach((file) => {
+          formData.append("uploaded_images", file);
+        });
+      } else {
+        formData.append(key, listingData[key]);
+      }
     });
 
     try {
-      // Send a POST request to the API to create a new listing.
       const { data } = await axiosReq.post("/listings/", formData);
       history.push(`/listings/${data.id}`);
     } catch (err) {
-      setErrors(err.response?.data);
       if (err.response?.status === 403) {
         history.push("/forbidden");
+      } else {
         setErrors(err.response?.data);
       }
     }
   };
 
+  if (userStatus === false) {
+    return <Forbidden403 />;
+  }
+
   return (
-    <>
-      {userStatus === false ? (
-        <Forbidden403 />
-      ) : (
-        <Form onSubmit={handleSubmit}>
-          <Row>
-            <Col className="py-2 p-0">
-              <Container
-                className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
-              >
-                <Form.Group className="text-center justify-content-between">
-                  {images ? (
-                    <>
-                      {Array.from(imageInput.current.files).map((file, idx) => (
-                        <figure key={idx}>
-                          <Image
-                            className={`"my-2 px-2" ${styles.Image}`}
-                            src={URL.createObjectURL(file)}
-                            rounded
-                          />
-                        </figure>
-                      ))}
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0">
+          <Container className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}>
+            <Form.Group className="text-center justify-content-between">
+              {listingData.images ? (
+                <>
+                  {Array.from(imageInput.current.files).map((file, idx) => (
+                    <figure key={idx}>
+                      <Image className={`"my-2 px-2" ${styles.Image}`} src={URL.createObjectURL(file)} rounded />
+                    </figure>
+                  ))}
 
-                      <div>
-                        <Form.Label
-                          className={`${btnStyles.Button} ${btnStyles.Bright} btn`}
-                          htmlFor="image-upload"
-                        >
-                          Change the image
-                        </Form.Label>
-                      </div>
-                    </>
-                  ) : (
-                    <Form.Label
-                      className="d-flex justify-content-center"
-                      htmlFor="image-upload"
-                    >
-                      <Asset
-                        src={upload}
-                        message="Click or tap to upload an image"
-                      />
+                  <div>
+                    <Form.Label className={`${btnStyles.Button} ${btnStyles.Bright} btn`} htmlFor="image-upload">
+                      Change the image
                     </Form.Label>
-                  )}
+                  </div>
+                </>
+              ) : (
+                <Form.Label className="d-flex justify-content-center" htmlFor="image-upload">
+                  <Asset src={upload} message="Click or tap to upload an image" />
+                </Form.Label>
+              )}
 
-                  <Form.File
-                    multiple
-                    id="image-upload"
-                    accept="image/*"
-                    onChange={handleChangeImage}
-                    ref={imageInput}
-                  />
-                </Form.Group>
-                {errors?.images?.map((message, idx) => (
-                  <Alert variant="warning" key={idx}>
-                    {message}
-                  </Alert>
-                ))}
+              <input type="file" multiple id="image-upload" accept="image/*" onChange={handleChangeImage} ref={imageInput} />
+            </Form.Group>
+            {errors?.images?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            {errors?.uploaded_images?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
 
-                {errors?.uploaded_images?.map((message, idx) => (
-                  <Alert variant="warning" key={idx}>
-                    {message}
-                  </Alert>
-                ))}
-
-                <div className="d-md-none">
-                  <ListingTextFields
-                    listingData={listingData}
-                    handleChange={handleChange}
-                    history={history}
-                    errors={errors}
-                    create={true}
-                  />
-                </div>
-              </Container>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12} className="d-none d-md-block p-0 p-md-2">
-              <Container className={appStyles.Content}>
-                <ListingTextFields
-                  listingData={listingData}
-                  handleChange={handleChange}
-                  history={history}
-                  errors={errors}
-                  create={true}
-                />
-              </Container>
-            </Col>
-          </Row>
-        </Form>
-      )}
-    </>
+            <div className="d-md-none">
+              <ListingTextFields listingData={listingData} handleChange={handleChange} history={history} errors={errors} create={true} />
+            </div>
+          </Container>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>
+            <ListingTextFields listingData={listingData} handleChange={handleChange} history={history} errors={errors} create={true} />
+          </Container>
+        </Col>
+      </Row>
+    </Form>
   );
 }
 
--- src/pages/listings/ListingEditForm.js
+++ src/pages/listings/ListingEditForm.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-
+import { useHistory, useParams } from "react-router-dom";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -10,27 +10,20 @@ import Image from "react-bootstrap/Image";
 import styles from "../../styles/ListingCreateEditForm.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
-
-import { useHistory, useParams } from "react-router-dom";
 import { axiosReq } from "../../api/axiosDefaults";
 import { useRedirect } from "../../hooks/useRedirect";
 import ListingTextFields from "../../components/ListingFormTextFields";
 import useUserStatus from "../../hooks/useUserStatus";
 import Forbidden403 from "../errors/Forbidden403";
+import upload from "../../assets/upload.png";
+import Asset from "../../components/Asset";
 
 function ListingEditForm() {
-  /**
-   * The ListingEditForm component is a functional component that renders a form for editing a listing.
-   * Imports the ListingTextFields component to render the input fields for the listing data.
-   * It contains images of the listing that can be deleted, and a form for uploading new images.
-   * It also contains a submit button that sends a request to the API to edit the listing.
-   * @returns {JSX.Element} - The JSX for the component.
-   */
-
   useRedirect("loggedOut");
+
   const [listingData, setListingData] = useState({
-    type: "apartment",
-    sale_type: "rent",
+    type: "",
+    sale_type: "",
     description: "",
     address_number: "",
     address_street: "",
@@ -48,32 +41,12 @@ function ListingEditForm() {
     energy_class: "A",
     construction_year: "",
     availability: "",
-    images: "",
+    images: [],
     uploaded_images: [],
+    latitude: "",
+    longitude: "",
+    amenities: [],
   });
-
-  const {
-    type,
-    sale_type,
-    description,
-    address_number,
-    address_street,
-    postcode,
-    city,
-    price,
-    surface,
-    levels,
-    bedrooms,
-    floor,
-    kitchens,
-    bathrooms,
-    living_rooms,
-    heating_system,
-    energy_class,
-    construction_year,
-    availability,
-    images,
-  } = listingData;
 
   const [errors, setErrors] = useState({});
   const imageInput = useRef(null);
@@ -83,69 +56,21 @@ function ListingEditForm() {
   const [selectedImages, setSelectedImages] = useState([]);
 
   useEffect(() => {
-    // Fetch the listing data from the API.
     const handleMount = async () => {
       try {
         const { data } = await axiosReq.get(`/listings/${id}/`);
-        const {
-          type,
-          sale_type,
-          description,
-          address_number,
-          address_street,
-          postcode,
-          city,
-          price,
-          surface,
-          levels,
-          bedrooms,
-          floor,
-          kitchens,
-          bathrooms,
-          living_rooms,
-          heating_system,
-          energy_class,
-          construction_year,
-          availability,
-          images,
-          uploaded_images,
-        } = data;
-
-        // Set the listing data state to the fetched data.
-        setListingData({
-          type,
-          sale_type,
-          description,
-          address_number,
-          address_street,
-          postcode,
-          city,
-          price,
-          surface,
-          levels,
-          bedrooms,
-          floor,
-          kitchens,
-          bathrooms,
-          living_rooms,
-          heating_system,
-          energy_class,
-          construction_year,
-          availability,
-          images,
-          uploaded_images,
-        });
+        setListingData(data);
       } catch (err) {
         if (err.response.status === 403) {
-          <Forbidden403 />;
+          history.push("/403");
+        } else {
+          setErrors(err.response?.data);
         }
-        // console.log(err);
       }
     };
     handleMount();
   }, [id, history]);
 
-  // Function to handle the change event for the input fields.
   const handleChange = (e) => {
     setListingData({
       ...listingData,
@@ -153,229 +78,169 @@ function ListingEditForm() {
     });
   };
 
-  // Function to handle the change event for the image input field.
   const handleChangeImage = (e) => {
     if (e.target.files.length) {
-      URL.revokeObjectURL(images);
+      URL.revokeObjectURL(listingData.images);
       setListingData({
         ...listingData,
-        image: URL.createObjectURL(e.target.files[0]),
+        images: [...listingData.images, URL.createObjectURL(e.target.files[0])],
       });
     }
   };
 
-  // Function to handle the selected images to delete
   const handleSelectedImages = (e) => {
-    // Get the checked checkboxes.
-    const checkedBoxes = document.querySelectorAll(
-      "input[name=images]:checked"
-    );
-    // If there are checked checkboxes, get their values and add them to the selected images state.
-    if (checkedBoxes.length > 0) {
-      const checkedValues = Array.from(checkedBoxes).map(
-        (checkbox) => checkbox.value
-      );
-
-      setSelectedImages((prevSelectedImages) => {
-        if (e.target.checked) {
-          return [...prevSelectedImages, ...checkedValues];
-        } else {
-          return prevSelectedImages.filter(
-            (image) => !checkedValues.includes(image)
-          );
-        }
-      });
+    const value = e.target.value;
+    if (e.target.checked) {
+      setSelectedImages([...selectedImages, value]);
     } else {
-      setSelectedImages([]);
+      setSelectedImages(selectedImages.filter((image) => image !== value));
     }
   };
 
-  // Function to handle the delete image event.
   const handleDeleteImage = async (e) => {
     e.preventDefault();
     try {
-      selectedImages.forEach(async (image) => {
-        await axiosReq.delete(`/listings/${id}/images/${image}/`);
-
-        setSelectedImages([]);
-        window.location.reload();
-      });
+      await Promise.all(
+        selectedImages.map((imageId) =>
+          axiosReq.delete(`/listings/${id}/images/${imageId}/`)
+        )
+      );
+      setSelectedImages([]);
+      window.location.reload();
     } catch (err) {
       setErrors(err.response?.data);
     }
   };
 
-  // Function to handle the submit event for the form.
   const handleSubmit = async (e) => {
     e.preventDefault();
     const formData = new FormData();
-    formData.append("type", type);
-    formData.append("sale_type", sale_type);
-    formData.append("description", description);
-    formData.append("address_number", address_number);
-    formData.append("address_street", address_street);
-    formData.append("postcode", postcode);
-    formData.append("city", city);
-    formData.append("price", price);
-    formData.append("surface", surface);
-    formData.append("levels", levels);
-    formData.append("bedrooms", bedrooms);
-    formData.append("floor", floor);
-    formData.append("kitchens", kitchens);
-    formData.append("bathrooms", bathrooms);
-    formData.append("living_rooms", living_rooms);
-    formData.append("heating_system", heating_system);
-    formData.append("energy_class", energy_class);
-    formData.append("construction_year", construction_year);
-    formData.append("availability", availability);
-    formData.append("images", imageInput.current.files[0]);
-    // Append the selected images to delete to the form data.
+
+    Object.entries(listingData).forEach(([key, value]) => {
+      if (Array.isArray(value)) {
+        value.forEach((item) => formData.append(key, item));
+      } else {
+        formData.append(key, value);
+      }
+    });
+
     if (imageInput.current.files.length > 0) {
       Array.from(imageInput.current.files).forEach((file) => {
         formData.append("uploaded_images", file);
       });
-    } else {
-      setErrors({ images: ["Please add an image"] });
     }
 
     try {
-      // Send a PUT request to the API to edit the listing.
       const { data } = await axiosReq.put(`/listings/${id}/`, formData);
-      // Redirect to the listing page for the edited listing.
-      window.scrollTo(0, 0);
-      window.localStorage.setItem("edited", true);
       history.push(`/listings/${data.id}`);
     } catch (err) {
       setErrors(err.response?.data);
       window.scrollTo(0, 0);
-      if (err.response?.status === 403) {
-        setErrors(err.response?.data);
-      }
     }
   };
 
+  if (userStatus === false) {
+    return <Forbidden403 />;
+  }
+
   return (
-    <>
-      {userStatus === false ? (
-        <Forbidden403 />
-      ) : (
-        <Form onSubmit={handleSubmit}>
-          <Row>
-            <Col className="py-2 p-0">
-              <Container
-                className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+
+    <Form onSubmit={handleSubmit}>
+      <Row>
+        <Col className="py-2 p-0">
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center justify-content-between">
+              <Alert
+                variant="danger"
+                className={`${btnStyles.Wide} mx-auto`}
               >
-                <Form.Group className="text-center justify-content-between">
-                  <>
-                    <Alert
-                      variant="danger"
-                      className={`${btnStyles.Wide} mx-auto`}
-                    >
-                      To delete images choose the ones you would like to delete
-                      and press the button below
-                    </Alert>
-                    {/* Map through the images and display them.
-                      The images are displayed in a grid and can be selected to delete them
-                      by checking the checkbox next to them. */}
-
-                    {Array.from(images).map((image) => (
-                      <figure key={image.id}>
-                        <input
-                          type="checkbox"
-                          name="images"
-                          value={image.id}
-                          id={image.url}
-                          onChange={handleSelectedImages}
-                        />
-                        <Image
-                          className={`"my-2 px-2" ${styles.Image}`}
-                          src={image.url}
-                          rounded
-                        />
-                      </figure>
-                    ))}
-                  </>
-                  <div>
-                    <button
-                      className={`${btnStyles.Button} ${btnStyles.Remove} btn mb-1`}
-                      onClick={handleDeleteImage}
-                    >
-                      Delete selected image(s)
-                    </button>
-                  </div>
-                  <>
-                    {/* Map through the selected images and display them. */}
-                    {images
-                      ? Array.from(imageInput.current.files).map(
-                          (file, idx) => (
-                            <figure key={idx}>
-                              <Image
-                                className={`my-2 px-2 ${styles.Image}`}
-                                src={URL.createObjectURL(file)}
-                                rounded
-                              />
-                            </figure>
-                          )
-                        )
-                      : null}
-
-                    <div>
-                      <Form.Label
-                        className={`${btnStyles.Button} ${btnStyles.Bright} btn`}
-                        htmlFor="image-upload"
-                      >
-                        Add image(s)
-                      </Form.Label>
-                    </div>
-                  </>
-
-                  <Form.File
-                    multiple
-                    id="image-upload"
-                    accept="image/*"
-                    onChange={handleChangeImage}
-                    ref={imageInput}
-                  />
-                </Form.Group>
-                {errors?.images?.map((message, idx) => (
-                  <Alert variant="warning" key={idx}>
-                    {message}
-                  </Alert>
+                To delete images choose the ones you would like to delete
+                and press the button below
+              </Alert>
+              <div>
+                {listingData.images.map((image, idx) => (
+                  <figure key={idx}>
+                    <input
+                      type="checkbox"
+                      name="images"
+                      value={image.id}
+                      onChange={handleSelectedImages}
+                    />
+                    <Image
+                      className={`my-2 px-2 ${styles.Image}`}
+                      src={image.url}
+                      rounded
+                    />
+                  </figure>
                 ))}
-
-                {Array.isArray(errors?.uploaded_images) &&
-                  errors?.uploaded_images.map((message, idx) => (
-                    <Alert variant="warning" key={idx}>
-                      {message}
-                    </Alert>
+              </div>
+              <button
+                className={`${btnStyles.Button} ${btnStyles.Remove} mb-1`}
+                onClick={handleDeleteImage}
+              >
+                Delete selected image(s)
+              </button>
+              <div>
+                {imageInput.current &&
+                  Array.from(imageInput.current.files).map((file, idx) => (
+                    <figure key={idx}>
+                      <Image
+                        className={`my-2 px-2 ${styles.Image}`}
+                        src={URL.createObjectURL(file)}
+                        rounded
+                      />
+                    </figure>
                   ))}
-
-                <div className="d-md-none">
-                  <ListingTextFields
-                    listingData={listingData}
-                    handleChange={handleChange}
-                    history={history}
-                    errors={errors}
-                  />
-                </div>
-              </Container>
-            </Col>
-          </Row>
-          <Row>
-            <Col md={12} className="d-none d-md-block p-0 p-md-2">
-              <Container className={appStyles.Content}>
-                <ListingTextFields
-                  listingData={listingData}
-                  handleChange={handleChange}
-                  history={history}
-                  errors={errors}
-                />
-              </Container>
-            </Col>
-          </Row>
-        </Form>
-      )}
-    </>
+                <Form.Label
+                  htmlFor="image-upload"
+                  className={styles.ImageUpload}
+                >
+                  <Asset src={upload} message="Click or tap to upload an image" />
+                </Form.Label>
+              </div>
+              <input
+                type="file"
+                multiple
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+                ref={imageInput}
+              />
+            </Form.Group>
+            {errors?.images?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            {errors?.uploaded_images?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <div className="d-md-none">
+              <ListingTextFields
+                listingData={listingData}
+                handleChange={handleChange}
+                errors={errors}
+              />
+            </div>
+          </Container>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={12} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>
+            <ListingTextFields
+              listingData={listingData}
+              handleChange={handleChange}
+              errors={errors}
+            />
+          </Container>
+        </Col>
+      </Row>
+    </Form>
   );
 }
 
