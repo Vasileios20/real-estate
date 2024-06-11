a16a598011fc311c818389cf429c61059a73f682
vasileios20 <vasilis_tsim@outlook.com>
2024-06-11 17:30:56 +0100

    refactor: Add language change functionality to ContactPage

--- src/pages/contact/ContactPage.js
+++ src/pages/contact/ContactPage.js
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ContactForm from "./ContactForm";
 import styles from "../../styles/ServicesPages.module.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
-import { Col } from "react-bootstrap";
+import Col from "react-bootstrap/Col";
+import { useTranslation } from "react-i18next";
 
 const ContactPage = () => {
+  const { t, i18n } = useTranslation();
+
+  const lng = navigator.language || navigator.userLanguage;
+
+  useEffect(() => {
+    i18n.changeLanguage(lng);
+  }, [i18n, lng]);
+
   return (
     <Container fluid className={`${styles.ContactPageContainer}`}>
       <Row className={`${styles.HeroImageCon}`}>
         <Col md={10} lg={8} className={`${styles.ContactPageContent} text-center`}>
-
-          Contact us today to learn more about our services and how we can assist you in achieving your real estate goals.
+          <p>{t("contactFormPage.p1") }</p>
         </Col>
         <Col sm={12} className={`${styles.ContactFormCol}`}>
           <ContactForm />
