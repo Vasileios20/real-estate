9000c4c69979f3f858c30303390e3646fb6b81cf
vasileios20 <vasilis_tsim@outlook.com>
2024-06-12 15:06:25 +0100

    refactor: Update ListingHeader component to handle type readiness before accessing translated values and fix conditional listing type render

--- src/components/ListingHeader.js
+++ src/components/ListingHeader.js
@@ -1,24 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "../styles/Listing.module.css";
 import { useTranslation } from "react-i18next";
 
 const ListingHeader = (props) => {
+  const [typeReady, setTypeReady] = useState(false);
 
   const { t, i18n } = useTranslation();
 
   const lng = navigator.language || navigator.userLanguage;
 
   useEffect(() => {
-
+    if (props.type !== undefined && props.sub_type !== undefined) {
+      setTypeReady(true);
+    }
     i18n.changeLanguage(lng);
-  }, [i18n, lng]);
+  }, [i18n, lng, props.type, props.sub_type]);
 
-  const saleType = props.sale_type === "rent" ? `${t("propertyDetails.typeRent")}` : `${t("propertyDetails.typeSale")}`;
+  const saleType = typeReady && props.sale_type === "rent" ? `${t("propertyDetails.typeRent")}` : `${t("propertyDetails.typeSale")}`;
 
-  const translatedType = t(`propertyDetails.types.${props.type}`);
+  const translatedType = typeReady && t(`propertyDetails.types.${props.type}`);
 
-  const translatedSubType = t(`propertyDetails.subTypes.${props.sub_type}`);
+  const translatedSubType = typeReady && t(`propertyDetails.subTypes.${props.sub_type}`);
   const municipality = lng === "en" ? props.municipality : props.municipality_gr;
   const county = lng === "en" ? props.county : props.county_gr;
   const region = lng === "en" ? props.region : props.region_gr;
@@ -35,7 +38,7 @@ const ListingHeader = (props) => {
           {t("propertyDetails.title", {
 
             sale_type: saleType,
-            type: translatedType === "residential" || translatedType === "commercial" ? translatedSubType : translatedSubType,
+            type: translatedType === "Land" ? translatedType : translatedSubType,
           })}, {municipality}, {county}, {region}
         </div>
         <div className={styles.Listing__fontawsome}>
